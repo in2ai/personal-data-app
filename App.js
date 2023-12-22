30c82d3cfc5d494afce3a4c0015d0f8536ca6c40
Adrián Ortega Rodríguez <adrian>
2023-12-22 17:33:43 +0100

    europass

--- App.js
+++ App.js
@@ -7,33 +7,63 @@ import {
   linkedinToDatabase,
   printLinkedinDatabase,
 } from "./src/utils/linkedin";
+import {
+  europassToDatabase,
+  printEuropassDatabase,
+} from "./src/utils/europass";
 
 export default function App() {
-  const onZipFileSelected = async (result) => {
+  const onLinkedinZipFileSelected = async (result) => {
     if (result) {
       const uri = result.assets[0].uri;
       await linkedinToDatabase(uri);
     }
   };
 
+  const onEuropassPdfFileSelected = async (result) => {
+    if (result) {
+      const uri = result.assets[0].uri;
+      await europassToDatabase(uri);
+    }
+  };
+
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
-        <Text>Personal Data App</Text>
-        <Button
-          title="Load Linkedin ZIP"
-          onPress={async () => {
-            const result = await DocumentPicker.getDocumentAsync({
-              type: "application/zip",
-              copyToCacheDirectory: true,
-            });
-            onZipFileSelected(result);
-          }}
-        />
-        <Button
-          title="Check Linkedin DB"
-          onPress={() => printLinkedinDatabase()}
-        />
+        <View style={styles.section}>
+          <Text>Linkedin</Text>
+          <Button
+            title="Load ZIP"
+            onPress={async () => {
+              const result = await DocumentPicker.getDocumentAsync({
+                type: "application/zip",
+                copyToCacheDirectory: true,
+              });
+              onLinkedinZipFileSelected(result);
+            }}
+          />
+          <Button
+            title="Print DB: linkedin table"
+            onPress={() => printLinkedinDatabase()}
+          />
+        </View>
+        <View style={styles.section}>
+          <Text>Europass</Text>
+          <Button
+            title="Load PDF"
+            onPress={async () => {
+              const result = await DocumentPicker.getDocumentAsync({
+                type: "application/pdf",
+                copyToCacheDirectory: true,
+              });
+              onEuropassPdfFileSelected(result);
+            }}
+          />
+          <Button
+            title="Print DB: europass table"
+            onPress={() => printEuropassDatabase()}
+          />
+        </View>
         <StatusBar style="auto" />
       </View>
     </SafeAreaProvider>
@@ -44,6 +74,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  section: {
+    margin: 10,
     alignItems: "center",
     justifyContent: "center",
   },
--- src/api/db.js
+++ src/api/db.js
@@ -3,11 +3,15 @@ import * as SQLite from "expo-sqlite";
 const connectDatabase = async () => {
   try {
     const db = SQLite.openDatabase("personal-data-app");
-    await db.execAsync([{ sql: `pragma foreign_keys = on;`, args: [] }], false);
+    //await db.execAsync([{ sql: `pragma foreign_keys = on;`, args: [] }], false);
     await db.execAsync(
       [
         {
           sql: `create table if not exists linkedin (id integer primary key not null, address text, birth_date text, first_name text, geo_location text, headline text, industry text, instant_messengers text, last_name text, maiden_name text, summary text, twitter_handles text, websites text, zip_code text);`,
+          args: [],
+        },
+        {
+          sql: `create table if not exists europass (id integer primary key not null, address text, birth_date text, first_name text, geo_location text, headline text, industry text, instant_messengers text, last_name text, maiden_name text, summary text, twitter_handles text, websites text, zip_code text);`,
           args: [],
         },
       ],
