100da6d2fae062c63fd1428aea6e4f54f5b6dc81
Daniel de Fuenmayor <fmixagent@gmail.com>
2024-02-15 06:56:46 +0700

    added recovering offers

--- src/screens/OffersScreen.tsx
+++ src/screens/OffersScreen.tsx
@@ -1,18 +1,99 @@
 import 'expo-dev-client';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 import { RootStackParamList } from '../navigation/MainNav';
+import { useAuthContext } from '../context-providers/auth-context';
+import { getEventsFromRelay, publishEventToRelay, signEvent } from '../api/nostr';
+import { WorkOffer } from '../models/WorkOffer';
 
-const screenContainerStyle = 'flex h-full w-full justify-center items-center p-5';
+const RELAY_URL = 'ws://137.184.117.201:8008';
+
+const screenContainerStyle = 'flex h-full w-full p-5';
 
 type OffersScreenProps = NativeStackScreenProps<RootStackParamList, 'Offers'>;
 
 const OffersScreen: React.FC<OffersScreenProps> = ({ navigation }) => {
+  const { publicKey, secretKey } = useAuthContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [offers, setOffers] = useState<WorkOffer[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getOffers();
+    return () => {
+      setIsLoading(false);
+    };
+  }, []);
+
+  const handlePublicMessage = async () => {
+    const message = '';
+    try {
+      const eventTemplate = {
+        kind: 1,
+        tags: [],
+        content: message,
+        created_at: Math.floor(Date.now() / 1000),
+      };
+      const signedEvent = signEvent(eventTemplate, secretKey);
+      await publishEventToRelay(RELAY_URL, signedEvent);
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  const insertOffer = async () => {
+    try {
+      const workOffer: WorkOffer = {
+        title: 'Oferta de trabajo',
+        summary: 'Oferta de trabajo de prueba',
+        requiredSkills: ['React', 'Node.js', 'MongoDB'],
+        location: 'Madrid',
+        price: 50000,
+        currency: 'EUR',
+        period: 'year',
+      };
+      const workOfferString = JSON.stringify(workOffer);
+
+      const eventTemplate = {
+        kind: 30023,
+        tags: [],
+        content: workOfferString,
+        created_at: Math.floor(Date.now() / 1000),
+      };
+      const signedEvent = signEvent(eventTemplate, secretKey);
+      await publishEventToRelay(RELAY_URL, signedEvent);
+      getOffers();
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  const getOffers = async () => {
+    console.log('//Getting offers: ', publicKey);
+    setIsLoading(true);
+    const events = await getEventsFromRelay(RELAY_URL, {
+      kinds: [30023],
+      limit: 10,
+    });
+    const offers: WorkOffer[] = events.map((event) => JSON.parse(event.content));
+    setIsLoading(false);
+    setOffers(offers);
+  };
+
   return (
     <View className={screenContainerStyle}>
-      <Text>Offers</Text>
+      {isLoading ? (
+        <View className="h-full w-full items-center justify-center">
+          <View className="mb-5">
+            <Text className="#3c7c8c">Recuperando ofertas...</Text>
+          </View>
+          <ActivityIndicator size="small" color="#3c7c8c" />
+        </View>
+      ) : (
+        <Text>{JSON.stringify(offers)}</Text>
+      )}
     </View>
   );
 };
