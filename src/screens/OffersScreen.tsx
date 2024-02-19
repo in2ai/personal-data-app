95056b689f4fc7d51b05b618d58d49ffc5b259e6
Daniel de Fuenmayor <fmixagent@gmail.com>
2024-02-19 20:38:44 +0700

    adapted offers screen to use model matching

--- src/screens/OffersScreen.tsx
+++ src/screens/OffersScreen.tsx
@@ -10,6 +10,10 @@ import { WorkOffer } from '../models/WorkOffer';
 import WorkOffersList from '../components/smart/WorkOffersList/WorkOffersList';
 import WorkOfferDetails from '../components/smart/WorkOffersList/WorkOfferDetails';
 
+import { matchCVOffer, initTfjs, load_model, jsonToSpaceDelimitedText } from '../api/dl';
+import { useUserDataContext } from '../context-providers/user-data-context';
+import CustomButton from '../components/smart/CustomButton';
+
 const RELAY_URL = 'ws://137.184.117.201:8008';
 
 const screenContainerStyle = 'flex h-full w-full';
@@ -17,40 +21,32 @@ const screenContainerStyle = 'flex h-full w-full';
 type OffersScreenProps = NativeStackScreenProps<RootStackParamList, 'Offers'>;
 
 const OffersScreen: React.FC<OffersScreenProps> = ({ navigation }) => {
+  const { userData } = useUserDataContext();
+
   const { publicKey, secretKey } = useAuthContext();
   const [isFetching, setIsFetching] = useState(false);
   const [workOffers, setWorkOffers] = useState<WorkOffer[]>([]);
   const [selectedWorkOffer, setSelectedWorkOffer] = useState<WorkOffer | null>(null);
 
+  // Machine learning model
+  const [modelLoading, setModelLoading] = useState(true);
+  const [model, setModel] = useState(null);
   useEffect(() => {
-    setIsFetching(true);
-    getOffers();
-    return () => {
-      setIsFetching(false);
-    };
-  }, []);
-
-  const handlePublicMessage = async () => {
-    const message = '';
-    try {
-      const eventTemplate = {
-        kind: 1,
-        tags: [],
-        content: message,
-        created_at: Math.floor(Date.now() / 1000),
-      };
-      const signedEvent = signEvent(eventTemplate, secretKey);
-      await publishEventToRelay(RELAY_URL, signedEvent);
-    } catch (e) {
-      alert(e.message);
+    async function initAndLoadModel() {
+      initTfjs();
+      const loadedModel = await load_model();
+      setModel(loadedModel);
+      setModelLoading(false);
+      getOffers();
     }
-  };
+    initAndLoadModel();
+  }, []);
 
   const insertOffer = async () => {
     try {
       const workOffer: WorkOffer = {
-        title: 'Oferta de trabajo',
-        summary: 'Oferta de trabajo de prueba',
+        title: 'Desarrollador Fullstack',
+        summary: 'Oferta para aplicaciones móviles y web',
         requiredSkills: ['React', 'Node.js', 'MongoDB'],
         location: 'Madrid',
         price: 50000,
@@ -81,11 +77,31 @@ const OffersScreen: React.FC<OffersScreenProps> = ({ navigation }) => {
     });
     const workOffers: WorkOffer[] = events.map((event) => ({
       ...JSON.parse(event.content),
-      match: Math.round(Math.random() * 100),
     }));
-    workOffers.sort((a, b) => (a.match ?? 0) - (b.match ?? 0)).reverse();
     setIsFetching(false);
     setWorkOffers(workOffers);
+
+    checkMatch();
+  };
+
+  const checkMatch = async () => {
+    if (!model) {
+      console.log('NO MODEL LOADED');
+      return;
+    }
+
+    const user = userData;
+    const offer = workOffers[0];
+
+    const user_string = jsonToSpaceDelimitedText(user);
+    const offer_string = jsonToSpaceDelimitedText(offer);
+
+    console.log('user_string', user_string);
+    console.log('offer_string', offer_string);
+
+    const match = await matchCVOffer(user_string, offer_string, model);
+
+    alert('Match with offer ' + match.toString() + '%');
   };
 
   const onRefreshList = () => {
@@ -106,8 +122,21 @@ const OffersScreen: React.FC<OffersScreenProps> = ({ navigation }) => {
     setSelectedWorkOffer(null);
   };
 
-  return (
+  return modelLoading ? (
+    <View className="flex h-full items-center justify-center">
+      <ActivityIndicator size="large" color="#3c7c8c" />
+      <Text className="mt-5 text-[#3c7c8c]">Loading model...</Text>
+    </View>
+  ) : (
     <View className={screenContainerStyle}>
+      <View className="flex-row">
+        <View className="w-1/2 p-4 pr-2">
+          <CustomButton buttonType="secondary" title="Insert offer" onPress={insertOffer} />
+        </View>
+        <View className="w-1/2 p-4 pl-2">
+          <CustomButton buttonType="secondary" title="Match" onPress={checkMatch} />
+        </View>
+      </View>
       {!selectedWorkOffer ? (
         <WorkOffersList
           workOffers={workOffers}
