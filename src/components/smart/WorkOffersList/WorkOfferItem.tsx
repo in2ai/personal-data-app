a1e60a9584187c072e9eddf7b31556f8ab31567b
Daniel de Fuenmayor <fmixagent@gmail.com>
2024-02-27 12:38:34 +0700

    fix: case match === 0

--- src/components/smart/WorkOffersList/WorkOfferItem.tsx
+++ src/components/smart/WorkOffersList/WorkOfferItem.tsx
@@ -28,7 +28,7 @@ const WorkOfferListItem: React.FC<WorkOfferListItemProps> = ({ workOffer, onPres
           <Text className="text-md text-defaultTextColor">{workOffer.summary}</Text>
         </View>
         <View className={workOffer.match ? matchColorView : null}>
-          {workOffer.match ? (
+          {workOffer.match || workOffer.match === 0 ? (
             <Text className={matchColorText}>{workOffer.match}%</Text>
           ) : (
             <ActivityIndicator size="small" color="#3c7c8c" />
