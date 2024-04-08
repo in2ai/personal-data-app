a13f7355578614d4db9639305954f461c7c32d3f
Caumel-In2AI <luis.caumel@in2ai.com>
2024-04-08 09:33:51 +0200

    [Develop/DL][Update] add checkoffer main method

--- src/context-providers/tensorflow-context.tsx
+++ src/context-providers/tensorflow-context.tsx
@@ -3,15 +3,6 @@ import { initTfjs, jsonToSpaceDelimitedText, load_model, matchCVOffer } from '..
 import { WorkOffer } from '../models/WorkOffer';
 import { useUserDataContext } from './user-data-context';
 
-//Borrar
-import {
-  getEventsFromRelay,
-  publishEventToRelay,
-  signEvent,
-} from "../api/nostr";
-const RELAY_URL = 'ws://137.184.117.201:8008';
-
-
 interface TensorflowContextInterface {
   isModelLoaded: boolean;
   checkOffer: (offer: WorkOffer) => Promise<number>;
@@ -48,40 +39,17 @@ const TensorflowContextProvider = (props: any) => {
       console.log('NO USER CV');
       return;
     }
+    // const user_string = jsonToSpaceDelimitedText(userData);
+    // const offer_string = jsonToSpaceDelimitedText(offer);
 
-    const user_string = jsonToSpaceDelimitedText(userData);
-    const offer_string = jsonToSpaceDelimitedText(offer);
+    // console.log('user_string', user_string);
+    // console.log('offer_string', offer_string);
 
-    console.log('user_string', user_string);
-    console.log('offer_string', offer_string);
-
-    const match = await matchCVOffer(user_string, offer_string, model);
+    const match = await matchCVOffer(userData, offer, model);
     return match;
   };
 
-  const checkOffer_v = async (): Promise<number> => {
-    if (!model) {
-      console.log('NO MODEL LOADED');
-      return;
-    }
-    if (!userData) {
-      console.log('NO USER CV');
-      return;
-    }
-    console.log("AAAAAAAAAAAAAAAAAAA")
-    const offers = await getEventsFromRelay(RELAY_URL, {
-      kinds: [30023],
-      limit: 1,
-    });
-
-    const user_string = userData;
-    const offer_string = JSON.parse(offers[0].content);
-
-    const match = await matchCVOffer(user_string, offer_string, model);
-    return match;
-  };
-
-  const api = { isModelLoaded, checkOffer, checkOffer_v };
+  const api = { isModelLoaded, checkOffer };
 
   return <TensorflowContext.Provider value={api}>{props.children}</TensorflowContext.Provider>;
 };
